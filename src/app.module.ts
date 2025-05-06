2ea550d500c5bb38004d9e43f1c4328c9dad9e0c
joshua <iuttn123@gmail.com>
2025-05-06 12:09:00 +0900

    docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { UsersModule } from './modules/users/users.module';
 import { GuestbooksModule } from './modules/guestbooks/guestbooks.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './config/typeOrm.cofing';
+import { typeOrmConfig } from './config/typeOrm.config';
 
 @Module({
   imports: [UsersModule, GuestbooksModule, AuthModule, TypeOrmModule.forRoot(typeOrmConfig)],
